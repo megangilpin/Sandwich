27601a451cbc9874993cd504021caed78c5c51a0
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-07-27 19:18:13 -0400

    fixed .env

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+require("dotenv").config();
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Welcome468",
+  password: process.env.SQL_PASSWORD,
   database: "sandwich_db"
 });
 
